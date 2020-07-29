f2a8100c1b2b0c9e98c3099eafe149361c4537cb
Kiryushin Denis <dkiryushin@sedmax.com>
2020-07-29 10:15:00 +0300

    delete B from tests

--- examples/simple_struct/simple_struct_test.go
+++ examples/simple_struct/simple_struct_test.go
@@ -8,11 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-type B struct {
-	i    int
-	flag bool
-}
-
 func TestA_String(t *testing.T) {
 	tests := []struct {
 		a       A
@@ -26,13 +21,12 @@ A{
 	i    int  42
 	flag bool true
 }`,
-			fmtWant: "simple_struct.B{i:42, flag:true}",
+			fmtWant: "simple_struct.A{i:42, flag:true}",
 		},
 	}
 	for _, tt := range tests {
-		b := B{i: tt.a.i, flag: tt.a.flag}
 		assert.Equal(t, tt.want, tt.a.String())
-		assert.Equal(t, tt.fmtWant, fmt.Sprintf("%#v", b))
+		assert.Equal(t, tt.fmtWant, fmt.Sprintf("%#v", tt.a))
 	}
 }
 
@@ -46,25 +40,18 @@ func Benchmark_A_String(b *testing.B) {
 	}
 }
 
-func Benchmark_B_fmt(b *testing.B) {
+func Benchmark_A_fmt(b *testing.B) {
 	b.ResetTimer()
 	for i := 0; i < 10000; i++ {
-		fmtA := randomB()
+		a := randomA()
 		b.StartTimer()
-		fmt.Sprintf("%#v", fmtA)
+		fmt.Sprintf("%#v", a)
 		b.StopTimer()
 	}
 }
 
 func randomA() A {
 	return A{
-		i:    rand.Int(),
-		flag: rand.Int()%2 == 0,
-	}
-}
-
-func randomB() B {
-	return B{
 		i:    rand.Int(),
 		flag: rand.Int()%2 == 0,
 	}
